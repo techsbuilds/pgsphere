a89cf2e489959835b6c594901d6f2edc0c2a6a79
rohitchauhan45 <chauhanrohith.3277@gmail.com>
2025-09-16 00:47:01 +0530

    Refactor code structure for improved readability and maintainability

--- backend/controller/customerController.js
+++ backend/controller/customerController.js
@@ -18,7 +18,7 @@ export const createCustomer = async (req, res, next) =>{
 
            if(!account) return res.status(404).json({message:"Account manager is not found.",success:false})
 
-           if(!account.branch.include(branch)) return res.status(403).json({message:"You are not authorized to add customer in this branch.",success:false})
+           if(!account.branch.includes(branch)) return res.status(403).json({message:"You are not authorized to add customer in this branch.",success:false})
 
         }
 
