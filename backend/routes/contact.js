d2e6b456a99687d0b150fb056445b0848af6eb97
mesuriyavivek7 <vivekmesuriya6@gmail.com>
2025-09-17 00:48:49 +0530

    change app to router

--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import { addContact ,getAllContacts} from '../controller/contactController.js'
 
-const app = express.Router()
+const router = express.Router()
+
+router.use((req, res, next) => {
+    console.log("📩 Contact route hit:", req.method, req.originalUrl);
+    next();
+});
 
 //For add contact
-app.post('/',addContact)
-app.get('/',getAllContacts)
-export default app
+router.post('/',addContact)
+router.get('/',getAllContacts)
+export default router
