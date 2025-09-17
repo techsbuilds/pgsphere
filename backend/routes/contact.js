53953a80710919cb6cbb9b5e29eb567063241fa6
mesuriyavivek7 <vivekmesuriya6@gmail.com>
2025-09-17 19:36:07 +0530

    added debugging command inside index

--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -3,11 +3,6 @@ import { addContact ,getAllContacts} from '../controller/contactController.js'
 
 const router = express.Router()
 
-router.use((req, res, next) => {
-    console.log("📩 Contact route hit:", req.method, req.originalUrl);
-    next();
-});
-
 //For add contact
 router.post('/',addContact)
 router.get('/',getAllContacts)
