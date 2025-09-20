637b820226951e31ca7b1d46c7b2f05e6c23b276
rohitchauhan45 <chauhanrohith.3277@gmail.com>
2025-09-20 17:46:55 +0530

    feat: add complaint and daily update functionality with email notifications for contact submissions

---
+++ backend/controller/dailyUpdateController.js
@@ -0,0 +1,87 @@
+import ACCOUNT from "../models/ACCOUNT.js"
+import DAILYUPDATE from "../models/DAILYUPDATE.js"
+
+export const addDailyUpdate = async (req, res, next) => {
+    try {
+        const { title, content_type, branch } = req.body
+        const { pgcode, userType, mongoid } = req
+
+        if (!title || !content_type || !branch) {
+            return res.status(400).json({ message: "Please Provide All Requireed Fields", success: false })
+        }
+
+        const newDailyUpdate = new DAILYUPDATE({
+            title,
+            content_type,
+            pgcode,
+            branch,
+            added_by: mongoid,
+            added_by_type: userType
+        })
+
+        await newDailyUpdate.save()
+
+        return res.status(201).json({ message: "Daily Update Added Successfully", success: true })
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const getAllDailyUpdatesbyBranch = async (req, res, next) => {
+    try {
+        const { pgcode } = req
+
+        const customer = await CUSTOMER.findOne({ _id: mongoid, status: true })
+
+        if (!customer) {
+            return res.status(404).json({ message: "Customer Not Found.", success: false })
+        }
+
+        const branch = customer.branch
+
+        const dailyUpdate = await DAILYUPDATE.find({ pgcode, branch }).populate('branch').sort({ createdAt: -1 })
+
+        return res.status(200).json({ message: "Getting Daily-update by Customer Successfully. ", data: dailyUpdate, success: true })
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const getAllDailyUpdate = async (req, res, next) => {
+    try {
+        const { pgcode, userType, mongoid } = req
+        const { branch } = req.query
+
+        let filter = {}
+
+        filter.pgcode = pgcode
+
+        if (userType === 'Account') {
+            const acmanager = await ACCOUNT.findOne({mongoid,pgcode})
+
+            if (!acmanager) {
+                return res.status(404).json({ message: "Acmanager Not Found", success: false })
+            }
+
+            const branches = acmanager.branch
+
+            if (branch) {
+                filter.branch = branch
+            } else {
+                filter.branch = { $in: branches }
+            }
+        } else {
+            if (branch) {
+                filter.branch = branch
+            }
+        }
+
+        const dailyData = await DAILYUPDATE.find(filter).populate('branch').sort({ createdAt: -1 })
+
+        return res.status(200).json({ message: "Getting DailyUpdate successfully.", data: dailyData, success: true })
+
+    } catch (error) {
+        next(error)
+    }
+}
---
+++ backend/models/DAILYUPDATE.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+//title content type [Maintainace, Notice, General] pgcode added_by added_by_type
+
+const dailyUpdateSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content_type: {
+        type: String,
+        required: true,
+        enum: ["Maintainace", "Notice", "General"]
+    },
+    pgcode: {
+        type: String,
+        required: true,             // enforce PG reference
+        ref: 'Loginmapping'         // pgcode lives in Loginmapping
+    },
+    branch: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Branch'
+    },
+    added_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        refPath: 'added_by_type',
+        required: true
+    },
+    added_by_type: {
+        type: String,
+        enum: ['Admin', 'Account'],
+        required: true
+    }
+}, { timestamps: true });
+
+export default mongoose.model('DailyUpdate', dailyUpdateSchema)
